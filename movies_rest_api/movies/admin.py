3d31ac84014672e53fe9bc6309f566c28037e5ca
Szymon Miks <szymon.miks@dlabs.pl>
2019-05-08 19:57:26 +0200

    Created models Movie and Comment
    Created models for Movie and Comments, added foreign key
    into Comment model. Register models into django admin panel.

--- movies_rest_api/movies/admin.py
+++ movies_rest_api/movies/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from movies.models import Movie, Comment
 
-# Register your models here.
+
+admin.site.register(Movie)
+admin.site.register(Comment)
--- movies_rest_api/movies/models.py
+++ movies_rest_api/movies/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=150)
+    created = models.DateTimeField(default=timezone.now, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "movies"
+
+
+class Comment(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    text = models.TextField()
+    created = models.DateTimeField(default=timezone.now, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return f'Comment for {self.movie.name}'
+
+    class Meta:
+        verbose_name_plural = "comments"
