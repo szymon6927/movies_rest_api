cb6a2d97d933aa9ac98e08219c58e50786987492
Szymon Miks <szymon.miks@dlabs.pl>
2019-05-11 17:20:24 +0200

    added MoveRating model to django admin panel

--- movies_rest_api/movies/admin.py
+++ movies_rest_api/movies/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from movies.models import Movie, Comment
+from movies.models import Movie, Comment, MovieRating
 
 
 admin.site.register(Movie)
 admin.site.register(Comment)
+admin.site.register(MovieRating)
