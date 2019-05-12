41b3d97eec0f8458c3f3c2c736f5bbf2fc52a6bc
Szymon Miks <szymon.miks@dlabs.pl>
2019-05-12 17:14:22 +0200

    Added domain to allowed_hosts

--- movies_rest_api/movies_rest_api/settings.py
+++ movies_rest_api/movies_rest_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['movies-netguru.szymonmiks.pl']
 
 # Application definition
 
