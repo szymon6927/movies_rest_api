6192b4d518203602cecbdc50cbbc72f8e3833b57
Szymon Miks <szymon.miks@dlabs.pl>
2019-05-12 17:00:40 +0200

    Changed project settings
    Changed settigs get secret_key and debug from .env file
    Added example env file

--- movies_rest_api/movies_rest_api/settings.py
+++ movies_rest_api/movies_rest_api/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a$y7y=4=@&r)2f8&8rarq$aiqr@6pzfaaq3!o!#!s2uym9pf%='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
